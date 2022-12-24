48a7f8dcfe186e01ca9205b1199864d810ca8413
SammyJoseph <sam.tab.paz@gmail.com>
2022-12-23 22:13:54 -0500

    después de la clase #3

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
+    // return "Bienvenido a la página Principal";
+});
+
+Route::get('cursos', function () {
+    return "Bienvenido a la página Cursos";
+});
+
+// Si la URL contiene crear-curso, se utiliza esta ruta gracias al orden
+Route::get('cursos/crear-curso', function () {
+    return "En esta página podrás crear un curso";
+});
+
+// En cambio, si la URL recibe otra variable diferente a crear-curso, se asume que es un Curso
+/* Recibir una variable por URL */
+/* Route::get('cursos/{curso}', function ($curso) {
+    return "Bienvenido al curso $curso desde cero";
 }); */
+
+/* Recibe dos variables */
+/* Route::get('cursos/{categoria_curso}/{curso}', function ($categoria_curso, $curso) {
+    return "Bienvenido al curso $curso de la categoría $categoria_curso";
+});
+ */
+
+/* Esta ruta reemplaza a las dos anteriores */
+/* el signo de ? después de categoria_curso indica que es opcional (se debe inicializar en null) */
+Route::get('cursos/{curso}/{categoria_curso?}', function ($curso, $categoria_curso = null) {
+    if ($categoria_curso) {
+        return "Bienvenido al curso $curso de la categoría $categoria_curso";
+    } else {
+        return "Bienvenido al curso $curso desde cero";
+    }
+    
+});
