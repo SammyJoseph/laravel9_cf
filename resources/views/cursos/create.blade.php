95a24f87487b696e29ec3585d538eca47bdf41d2
Sammy Joseph <sam.tab.paz@gmail.com>
2022-12-26 14:33:58 -0500

    en la clase #18

--- resources/views/cursos/create.blade.php
+++ resources/views/cursos/create.blade.php
@@ -19,17 +19,14 @@
                         <form action="forms/contact.php" method="post" role="form" class="php-email-form">
                         <div class="row">
                             <div class="col-md-6 form-group">
-                            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                                <input type="text" name="name" class="form-control" id="name" placeholder="Nombre" required>
                             </div>
                             <div class="col-md-6 form-group mt-3 mt-md-0">
-                            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                                <input type="text" class="form-control" name="category" id="category" placeholder="Categoría" required>
                             </div>
                         </div>
                         <div class="form-group mt-3">
-                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-                        </div>
-                        <div class="form-group mt-3">
-                            <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                            <textarea class="form-control" name="message" rows="5" placeholder="Descripción" required></textarea>
                         </div>
                         <div class="my-3">
                             <div class="loading">Loading</div>
