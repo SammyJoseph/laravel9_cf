68f35e170bb8e98a83dfee450163e6239c8b0120
Sammy Joseph <83559107+SammyJoseph@users.noreply.github.com>
2022-12-27 11:26:45 -0500

    error fixed

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -19,7 +19,7 @@
                 <li>
                     {{-- <a href="cursos/{{$c->name}}"> --}}
                     {{-- <a href="{{route('cursos.show', $c->id)}}"> --}}
-                    <a href="{{route('cursos.show', $c)}}">
+                    <a href="{{route('cursos.show', $curso)}}">
                         {{$c->name}}
                     </a>
                 </li>
